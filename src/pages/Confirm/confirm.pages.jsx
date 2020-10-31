29fd57427489fb9017d5588fe33ec02b726dfefd
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-10-31 15:34:43 -0400

    feat: Confirm page

---
+++ src/pages/Confirm/confirm.pages.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import {Button} from "@material-ui/core";
+const Confirm = () => {
+    return(
+        <div>
+            <p>Here’s your plan to completeing this goal</p>
+            <h5>Step: </h5>
+            <h5>Step: </h5>
+
+            <h5>Looks Good?</h5>
+
+
+            <div>
+                <Button color="secondary">No</Button>
+                <Button>Yes</Button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Confirm
