00048ce88de344a738bfa6526dd10d8e73ce230b
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-10-31 14:21:26 -0400

    feat: Goal Duration page

--- src/pages/GoalDuration/goalduration.page.jsx
+++ src/pages/GoalDuration/goalduration.page.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react"
 import {Button} from "@material-ui/core";
 
-const GoalDuration = ({goal}) => {
+const GoalDuration = ({currentGoal}) => {
     const [days, setDays] = useState(1);
 
     return(
         <div>
             <div>
             <p>How many days will this task take to complete?</p>
-            <h1>{goal}</h1>
+            <h1>{currentGoal}</h1>
             <h2>{days < 0 ? setDays(0) : days }</h2>
            <Button onClick={() => setDays(days-1)}>-</Button>
            <Button onClick={() => setDays(days+1)}>+</Button>  
